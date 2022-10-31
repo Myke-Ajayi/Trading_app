2df5622cba6f492bd9aa6564081a51098eda9431
Michael Ajayi <mike.endowed@icloud.com>
2022-10-31 15:59:27 -0400

    frame work for  the app

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 
-BASE_URL = 'https://paper-api.alpaca.markets'
-API_KEY_ID = 'PKZPGQRABOBIJ2AXU7ML'
-SECRET_KEY = 'pGcW6VPfmaPLZGrzewfMLN08hRtNZbMmDZ0ndn7a'
-DB_FILE = '/Users/king_michael/projects/Trading_app/app.db'
-POLYGON_API = 'q5wyp5YZlSEVFXKudYrpzHwuN9MxZXB9'
+BASE_URL = '************'
+API_KEY_ID = '*************'
+DB_FILE = '************'
+POLYGON_API = '************'
