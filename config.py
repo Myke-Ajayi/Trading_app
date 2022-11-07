f920183781f742760346321c4ced80c96275ff91
Michael Ajayi <mike.endowed@icloud.com>
2022-11-06 21:27:24 -0500

    m

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 BASE_URL = 'https://paper-api.alpaca.markets'
-API_KEY_ID = 'PKZPGQRABOBIJ2AXU7ML'
-SECRET_KEY = 'pGcW6VPfmaPLZGrzewfMLN08hRtNZbMmDZ0ndn7a'
+API_KEY_ID = '******************'
+SECRET_KEY = '*********************************'
 DB_FILE = '/Users/king_michael/projects/Trading_app/app.db'
-POLYGON_API = 'q5wyp5YZlSEVFXKudYrpzHwuN9MxZXB9'
+POLYGON_API = '********************'
